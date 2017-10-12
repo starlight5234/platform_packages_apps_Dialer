757228b62fc7dc9a735cf8b078308f323b18e388
Abhishek Panjikar <apanji@codeaurora.org>
2017-10-12 18:58:26 +0530

    IMS-VT: Exception while querying for activity for a detached fragment.
     - Exception occurs when user downgrades the call by switching off the mobile
       data for few  iterations.
    
     - When user disables mobile data, video capabilities are lost and the call
       is downgraded to Volte. This detaches VideoCallFragment and InCallFragment
       is attached. After the transition, onGolbalLayout is called which in turn
       tries to get the activity context for detatched VideoCallFragment. As the
       VideoCallFragment is detached, getActivity returns a null and app throws
       Null Pointer Exception.
    
     - Added a check to determine if VideoCallFragment is still attached to
       activity after onGolbalLayout is called.
    
    Change-Id: I4bbf7d3369f4f225464748c54799703af6d205a0
    CRs-Fixed: 2340742

--- java/com/android/incallui/video/impl/VideoCallFragment.java
+++ java/com/android/incallui/video/impl/VideoCallFragment.java
@@ -1248,6 +1248,12 @@ public class VideoCallFragment extends Fragment
   }
 
   private void updatePreviewVideoScaling() {
+    if (!isAdded()) {
+      LogUtil.i(
+         "VideoCallFragment.updatePreviewVideoScaling","fragment not attached");
+      return;
+    }
+
     if (previewTextureView.getWidth() == 0 || previewTextureView.getHeight() == 0) {
       LogUtil.i("VideoCallFragment.updatePreviewVideoScaling", "view layout hasn't finished yet");
       return;
