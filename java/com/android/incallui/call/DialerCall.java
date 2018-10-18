dbfea825871fba81e3207a0edc5518e85ea8b336
qiongz <qiongz@codeaurora.org>
2018-10-18 17:00:27 +0800

    Dialer: Fix the issue that call back number is not reset after ECBM.
    Call back number will set to phone number when device is in ECBM
    with property PROPERTY_EMERGENCY_CALLBACK_MODE of the call. And
    the call back number will always be shown even exit ECBM.
    To fix it, reset call back number to empty when the device is not
    in ECBM.
    
    Change-Id: Ib00eb95ab48b2b49ca3b32c25064b978f32cd4fa
    CRs-Fixed: 2355730

--- java/com/android/incallui/call/DialerCall.java
+++ java/com/android/incallui/call/DialerCall.java
@@ -1558,13 +1558,12 @@ public class DialerCall implements VideoTechListener, StateChangedListener, Capa
   }
 
   public String getCallbackNumber() {
+    // Show the emergency callback number if either:
+    // 1. This is an emergency call.
+    // 2. The phone is in Emergency Callback Mode, which means we should show the callback
+    //    number.
+    boolean showCallbackNumber = hasProperty(Details.PROPERTY_EMERGENCY_CALLBACK_MODE);
     if (callbackNumber == null) {
-      // Show the emergency callback number if either:
-      // 1. This is an emergency call.
-      // 2. The phone is in Emergency Callback Mode, which means we should show the callback
-      //    number.
-      boolean showCallbackNumber = hasProperty(Details.PROPERTY_EMERGENCY_CALLBACK_MODE);
-
       if (isEmergencyCall() || showCallbackNumber) {
         callbackNumber =
             context.getSystemService(TelecomManager.class).getLine1Number(getAccountHandle());
@@ -1573,6 +1572,8 @@ public class DialerCall implements VideoTechListener, StateChangedListener, Capa
       if (callbackNumber == null) {
         callbackNumber = "";
       }
+    } else if (!showCallbackNumber) {
+        callbackNumber = "";
     }
     return callbackNumber;
   }
