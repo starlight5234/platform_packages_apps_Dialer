b727b5b091686f6c2cc16b368a5403aa0d462577
Umashankar Godachi <umasha@codeaurora.org>
2018-10-30 17:46:50 +0530

    Display InCallUI on auto answer
    When a MT call is auto answered bring the InCallUI to
    foreground by starting the InCallActivity.
    
    Change-Id: I77e21f41699fa3ff78dd793d2f226ad95d11497f
    CRs-Fixed: 2358268

--- java/com/android/incallui/InCallPresenter.java
+++ java/com/android/incallui/InCallPresenter.java
@@ -1502,6 +1502,16 @@ public class InCallPresenter implements CallList.Listener, AudioModeProvider.Aud
         "InCallPresenter.startOrFinishUi", "startOrFinishUi: " + inCallState + " -> " + newState);
 
     // TODO: Consider a proper state machine implementation
+    // If the call is auto answered bring up the InCallActivity
+    boolean isAutoAnswer = false;
+
+    if ((callList.getDisconnectedCall() == null) &&
+            (callList.getDisconnectingCall() == null)) {
+        isAutoAnswer = (inCallState == InCallState.INCOMING) &&
+                (newState == InCallState.INCALL) && (inCallActivity == null);
+    }
+
+    Log.d(this, "startOrFinishUi: " + isAutoAnswer);
 
     // If the state isn't changing we have already done any starting/stopping of activities in
     // a previous pass...so lets cut out early
@@ -1594,7 +1604,7 @@ public class InCallPresenter implements CallList.Listener, AudioModeProvider.Aud
       inCallActivity.dismissPendingDialogs();
     }
 
-    if ((showCallUi || showAccountPicker) && !shouldStartInBubbleMode()) {
+    if ((showCallUi || showAccountPicker || isAutoAnswer) && !shouldStartInBubbleMode()) {
       LogUtil.i("InCallPresenter.startOrFinishUi", "Start in call UI");
       showInCall(false /* showDialpad */, !showAccountPicker /* newOutgoingCall */);
     } else if (newState == InCallState.NO_CALLS) {
