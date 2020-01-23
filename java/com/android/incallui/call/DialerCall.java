28998165ddc8f7a205faf1ce5847f4af8c4ed829
Nazish Tabassum <ntabassu@codeaurora.org>
2020-01-23 11:56:46 +0530

    Initialize callProviderLabel with an empty string
    current logic does not initialize callProviderLabel.
    This leads showing "calling via null" incall UI in case of
    MO emergency call with no sim card inserted.
    
    Change-Id: Ie70404fdfd3aadf4c4ed1f7e2cd04b4f21f8555d
    CRs-Fixed: 2608723

--- java/com/android/incallui/call/DialerCall.java
+++ java/com/android/incallui/call/DialerCall.java
@@ -690,6 +690,7 @@ public class DialerCall implements VideoTechListener, StateChangedListener, Capa
       phoneAccountHandle = newPhoneAccountHandle;
 
       if (phoneAccountHandle != null) {
+        callProviderLabel = "";
         PhoneAccount phoneAccount = telecomManager.getPhoneAccount(phoneAccountHandle);
         if (phoneAccount != null) {
           isCallSubjectSupported =
