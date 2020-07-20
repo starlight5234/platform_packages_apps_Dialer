f9cb640a4c8a228f503e429b50c8d4013a42d0c4
Sneh Bansal <snehb@codeaurora.org>
2020-07-20 21:11:02 +0530

    Add flag to makefiles to prevent compilation
    Add TARGET_NO_TELEPHONY flag check to makefile of proprietary telephony
    modules to facilitate disabling of such components at compile-time for
    targets that do not support telephony.
    
    Change-Id: If591b9d9f7934a3566face518028209abd5b3a5f
    CRs-Fixed: 2723112

--- Android.mk
+++ Android.mk
@@ -9,7 +9,9 @@
 #      $ protoc --plugin=protoc-gen-grpc-java=prebuilts/tools/common/m2/repository/io/grpc/protoc-gen-grpc-java/1.0.3/protoc-gen-grpc-java-1.0.3-linux-x86_64.exe \
 #               --grpc-java_out=lite:"packages/apps/Dialer/java/com/android/voicemail/impl/" \
 #               --proto_path="packages/apps/Dialer/java/com/android/voicemail/impl/transcribe/grpc/" "packages/apps/Dialer/java/com/android/voicemail/impl/transcribe/grpc/voicemail_transcription.proto"
+ifneq ($(TARGET_NO_TELEPHONY), true)
 ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS), true)
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -502,4 +504,6 @@ LOCAL_JAVA_LANGUAGE_VERSION := 1.8
 include $(BUILD_HOST_JAVA_LIBRARY)
 
 include $(CLEAR_VARS)
+
 endif
+endif # TARGET_NO_TELEPHONY
